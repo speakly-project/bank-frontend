abd913cbd2139ac83fbd3a77dde59ec84c222762
Alberto Rubio F.dez <alberto13101@gmail.com>
2026-01-08 12:06:38 +0100

    feat: Implementación de providers

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,6 +5,8 @@ import { routes } from './app.routes';
 import localeEs from '@angular/common/locales/es';
 import { registerLocaleData } from '@angular/common';
 import { LOCALE_ID,DEFAULT_CURRENCY_CODE } from '@angular/core';
+import { provideHttpClient, withInterceptorsFromDi, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthInterceptor } from './services/auth-interceptor';
  
 registerLocaleData(localeEs);
 
@@ -13,6 +15,8 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideHttpClient(withInterceptorsFromDi()),
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
     {provide: LOCALE_ID, useValue: 'es-ES' },
     {provide: DEFAULT_CURRENCY_CODE, useValue: 'EUR' }
   ]
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,15 @@
 import { Routes } from '@angular/router';
-import { Cursos } from './components/pages/cursos/cursos';
 import { Home } from './components/pages/inicio/home';
 import { loginGuard } from './guards/login-guard';
 import { Error } from './components/pages/error/error';
 import { Login } from './components/pages/login/login';
 import { Logout } from './components/pages/logout/logout';
-import { Users } from './components/pages/users/users';
 
 export const routes: Routes = [
     // { path: '', component: Home, canActivate: [loginGuard] },
     // { path: 'cursos', component: Cursos, canActivate: [loginGuard] },
     // { path: 'users', component: Users, canActivate: [loginGuard] },
     { path: '', component: Home},
-    { path: 'cursos', component: Cursos},
-    { path: 'users', component: Users},
     { path: 'login', component: Login},
     { path: 'logout', component: Logout },
     { path: '**', redirectTo: '' },
