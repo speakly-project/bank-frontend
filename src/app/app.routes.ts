f65d2c345df87bf8b4b237933011f15fc7775926
EnriqueMislata <soyenriqueh@hotmail.com>
2026-01-14 08:56:34 +0100

    refactor: quitado pagina bank

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,10 @@ import { Home } from './components/pages/home/home';
 import { loginGuard } from './guards/login-guard';
 import { Login } from './components/pages/login/login';
 import { Logout } from './components/pages/logout/logout';
-import { Bank } from './components/pages/bank/bank';
 import { AccountDetail } from './components/pages/accounts/account-detail/account-detail';
 
 export const routes: Routes = [
     { path: '', component: Home, canActivate: [loginGuard] },
-    { path: 'bank', component: Bank, canActivate: [loginGuard] },
     { path: 'accounts/:iban', component: AccountDetail, canActivate: [loginGuard] },
     //{ path: 'users', component: Account, canActivate: [loginGuard] },
     { path: 'login', component: Login},
